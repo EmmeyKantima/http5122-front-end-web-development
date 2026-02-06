96bba3876b05d029aa32fc1db957bbc87307dce7
Kantima Pinthong <kantima.pinthong@gmail.com>
2026-02-05 20:31:13 -0500

    add flies and modified README

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # http5122-Front-End
 Class content from Front-End Web Development
 
-Make first change //20 Jan !!
+
+***HTTP5122 - Front-End Web Development*** is a class primarily focuses on learning to code in JavaScript.
+
+[JavaScript-w3s](https://www.w3schools.com/js/)
+
+>Week3 Loop e.g.
+
+![javascript](images/screenshot-javascript.png)
+
+>Week 4 Fuction e.g.
+
+```javascript
+function averageOfNumbers(num1, num2, num3, num4, num5){
+    var sum = num1+num2+num3+num4+num5;
+    return ((sum/5).toFixed(1));
+}
+var avg= averageOfNumbers(5, 10, 15, 20, 25);
+```
---
+++ wk4/lab-4-2.js
@@ -0,0 +1,36 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2:  AN AVERAGE FUNCTION
+
+
+//################## CREATE YOUR AVERAGE FUNCTION
+//This function takes five numbers and returns their average to one decimal place.
+
+function averageOfNumbers(num1, num2, num3, num4, num5){
+    var sum = num1+num2+num3+num4+num5;
+    return ((sum/5).toFixed(1));
+    
+}
+var avg= averageOfNumbers(5, 10, 15, 20, 25);
+
+
+console.log(avg);
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+
+var http5125 = 85;
+var http5114 = 80;
+var http5121 = 69;
+var http5110 = 90;
+var http5126 = 90;
+
+
+
+var avgGrade= averageOfNumbers(http5125,http5114,http5121,http5110,http5126)
+
+if(avgGrade >= 70){
+    alert("success")
+}
+else{
+    alert("Review Required")
+} 
+
