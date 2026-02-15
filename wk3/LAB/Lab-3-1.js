6ed2b465bfdc7ea48cfafca30c9140cb27b4b0af
Kantima Pinthong <kantima.pinthong@gmail.com>
2026-02-15 14:04:05 -0500

    deleted flies

--- wk3/LAB/Lab-3-1.js
+++
@@ -1,8 +0,0 @@
-//LAB 3 - ARRAYS & LOOPS - PART 1
-
-
-//ARRAY OF FRUITS
-var fruits = ["mango", "apple", "orenge", "watermelon", "banana"];
-
-//OUTPUT ONE FRUIT FROM THE ARRAY IN POPUP BOX
-alert(fruits [3]);
